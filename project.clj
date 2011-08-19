02ea610437c825711396259a27449e6d3143638f
meetup <meetup@ip-10-73-21-212.ec2.internal>
2011-08-19 00:28:21 +0000

    mark 0.1.0-alpha1; describe in project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jrill "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject jrill "0.1.0-alpha1"
+  :description "Clojure wrapper for JXPath from Apache Commons"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [commons-jxpath "1.3"]])
