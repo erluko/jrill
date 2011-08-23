7c92b767751eb80f0d8ebcc11fbed5d13e3b5534
Eric Kobrin <erluko+git@gmail.com>
2011-08-23 07:35:16 -0400

    added some tests

--- test/jrill/test/core.clj
+++ test/jrill/test/core.clj
@@ -1,6 +1,25 @@
 (ns jrill.test.core
-  (:use [jrill.core])
+  (:refer-clojure :exclude [get])
+  (:require [jrill.core :as jrill])
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest gets-string-int-methods
+  (is (= '("hashCode" "compareTo" "compareTo"
+	   "indexOf" "indexOf" "indexOf"
+	   "indexOf" "codePointAt"
+	   "codePointBefore" "codePointCount"
+	   "compareToIgnoreCase" "lastIndexOf"
+	   "lastIndexOf" "lastIndexOf"
+	   "lastIndexOf" "length"
+	   "offsetByCodePoints")
+	 (jrill/gets String "methods[returnType='int']/name"))))
+
+
+(deftest gets-map
+  (is (= '("A" "C")
+	 (jrill/gets
+		 {"a" {"f" 1 "n" "A"}
+		  "b" {"f" 0 "n" "B"}
+		  "c" {"f" 1 "n" "C"}}
+		 "/*[f=1]/n"))))
+
